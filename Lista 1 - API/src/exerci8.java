a0b97067e9ed6adfef42a3b6dcea27ae691388fa
JhonnyGama <122324709+JhonnyGama@users.noreply.github.com>
2024-03-11 11:31:50 -0300

    quarto commit

--- Lista 1 - API/src/exerci8.java
+++ Lista 1 - API/src/exerci8.java
@@ -4,21 +4,19 @@ public class exerci8 {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		double pesoSacoKg,quantidadeDiariaPorGato,pesoSacoG,restante,consumoTotal;
+		double pesoSacoKg,quantidadeDiariaPorGato,consumoTotal;
 		
         // Recebendo o peso do saco de ração em quilogramas
         System.out.println("Digite o peso do saco de ração em quilogramas (Kg): ");
         pesoSacoKg = sc.nextDouble();
         System.out.println("Digite a quantidade de ração fornecida para cada gato por dia em gramas (g): ");
-        quantidadeDiariaPorGato = sc.nextInt();
+        quantidadeDiariaPorGato = sc.nextDouble();
         
         // Calculo
-        consumoTotal = (quantidadeDiariaPorGato * 2) * 5;
-        pesoSacoG = pesoSacoKg * 1000;
-        restante = pesoSacoG - consumoTotal;
+        consumoTotal = pesoSacoKg - quantidadeDiariaPorGato / 1000 * 2 * 5;
 
         // Mostra o resultado
-        System.out.printf("Após cinco dias, restarão %.2f gramas de ração no saco.", restante);
+        System.out.printf("Após cinco dias, restarão %.2f KG de ração no saco.", consumoTotal);
 
 	}
 
